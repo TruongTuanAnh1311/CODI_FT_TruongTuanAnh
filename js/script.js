0963a176ed0161319b7a7da6b0306dba39dc354c
TruongTuanAnh1311 <tanhne131199@gmail.com>
2023-08-14 22:17:10 +0700

    commit lab 1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Page : onload
+// alert("helo Tanh");
+//var name= prompt("Nhập Tên");
+//alert('Tôi tên là : '+name);
+//const div = document.getElementsByClassName("div1")
+//datatype div:typeof()
+// object obbject.property = 
+//array
+//function -> hàm
+// function ten_ham
+//let khai báo biến cục bộ
+// biến cục bộ thì sẽ không thể sử dụng ngoài function
+//var tenbien : khai báo biến toàn cục
+
+var name =''
+
+function function1(){
+    let name = document.getElementById('name').value;
+    console.log(name);
+    //const div = document.getElementsByClassName('div1');
+}
